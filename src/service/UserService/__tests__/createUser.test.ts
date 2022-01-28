b65f7bd2ab898f8b00699f5820ce132cd2e62647
eddyzera <edgar.nascimento@newgo.com.br>
2022-01-28 13:55:47 -0300

    [test]: validate the email of user and verify to be correct

--- src/service/UserService/__tests__/createUser.test.ts
+++ src/service/UserService/__tests__/createUser.test.ts
@@ -2,6 +2,9 @@ import { CreateUser } from '../createUser'
 import { CreateUserMockRepository } from './mocks/CreateUserMockRepository'
 
 let storeValid
+let storeEmailInvalid
+let createRepository
+let createUserService
 describe('CreateUser', () => {
   beforeAll(() => {
     storeValid = {
@@ -9,14 +12,32 @@ describe('CreateUser', () => {
       password: '1234@Teste',
       name: 'John Doe'
     }
+
+    storeEmailInvalid = {
+      email: 'esilvateste622gmail.com',
+      password: '1234@Teste',
+      name: 'John Doe'
+    }
+
+    createRepository = new CreateUserMockRepository()
+    createUserService = new CreateUser(createRepository)
   })
   it('should create a user with the data, email, password, name, lastName', async () => {
-    const createRepository = new CreateUserMockRepository()
-    const createUserService = new CreateUser(createRepository)
     const result = await createUserService.execute(storeValid)
     const expected = {
       message: 'Create user with success',
       statusCode: 201
+    }
+
+    expect(result).toEqual(expected)
+  })
+
+  it('should validate email and if the email of user to be invalid return a object of failed email', async () => {
+    const result = await createUserService.execute(storeEmailInvalid)
+
+    const expected = {
+      message: 'Email invalid',
+      statusCode: 401
     }
 
     expect(result).toEqual(expected)
