e861ce22a7966d0309437eb133cebeb48deb50fd
eddyzera <edgar.nascimento@newgo.com.br>
2022-01-28 13:46:05 -0300

    [test]: refactoring test of CreateUser

--- src/service/UserService/__tests__/createUser.test.ts
+++ src/service/UserService/__tests__/createUser.test.ts
@@ -1,10 +1,10 @@
 import { CreateUser } from '../createUser'
 import { CreateUserMockRepository } from './mocks/CreateUserMockRepository'
 
-let store
+let storeValid
 describe('CreateUser', () => {
   beforeAll(() => {
-    store = {
+    storeValid = {
       email: 'esilvateste622@gmail.com',
       password: '1234@Teste',
       name: 'John Doe'
@@ -13,7 +13,7 @@ describe('CreateUser', () => {
   it('should create a user with the data, email, password, name, lastName', async () => {
     const createRepository = new CreateUserMockRepository()
     const createUserService = new CreateUser(createRepository)
-    const result = await createUserService.execute(store)
+    const result = await createUserService.execute(storeValid)
     const expected = {
       message: 'Create user with success',
       statusCode: 201
